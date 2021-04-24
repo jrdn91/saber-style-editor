9a27c32c4bdf50167da9ddde07df5431d307234a
Jordan Riser <riser.jordan@gmail.com>
2021-04-24 09:41:20 -0400

    adds description to audio flicker

--- src/SaberStyles/AudioFlicker.js
+++ src/SaberStyles/AudioFlicker.js
@@ -1,5 +1,7 @@
 const styleObject = {
   title: "AudioFlicker",
+  description:
+    "Mixes between A and B based on audio. Quiet audio means more A, loud audio means more B. Based on a single sample instead of an average to make it flicker.",
   properties: [
     {
       title: "Color A",
