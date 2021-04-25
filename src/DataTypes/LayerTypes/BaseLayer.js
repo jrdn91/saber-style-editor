d277aebd6bc6ee8a1907d0c55065339b3da3f3f6
Jordan Riser <riser.jordan@gmail.com>
2021-04-25 16:50:48 -0400

    adds baselayer

--- src/DataTypes/LayerTypes/BaseLayer.js
+++ src/DataTypes/LayerTypes/BaseLayer.js
@@ -15,18 +15,51 @@ saberStylesReq.keys().forEach((x) => {
   SaberStyles[saberStyleName[1]] = saberStylesReq(x).default
 })
 
+export const ValueTypes = {}
+const valueTypesReq = require.context(
+  "../ValueTypes",
+  true,
+  /^(.*\.(js))[^.]*$/im
+)
+valueTypesReq.keys().forEach((x) => {
+  const saberStyleName = x.match(/\.\/([A-Za-z]+).js/)
+  ValueTypes[saberStyleName[1]] = valueTypesReq(x).default
+})
+
+const BaseLayerProperty = t
+  .model("BaseLayerProperty", {
+    id: t.optional(t.identifier, uuidv4()),
+    title: t.optional(t.string, "Property"),
+    token: t.optional(t.string, ":property:"),
+    value: t.union(
+      Color,
+      Rgb,
+      t.reference(Token),
+      t.union(...Object.values(SaberStyles))
+    ),
+  })
+  .actions((self) => ({
+    updateValue(newValue) {
+      self.value = newValue
+    },
+    getCode() {
+      return self.value.getCode()
+    },
+  }))
+
 const BaseLayer = t.model("BaseLayer", {
   id: t.optional(t.identifier, uuidv4()),
   title: t.optional(t.string, "BaseLayer"),
   description:
     "This is the Base Layer for your Saber and usually holds the color for your Saber",
   token: t.optional(t.string, ":layer:"),
-  value: t.union(
-    Color,
-    Rgb,
-    t.reference(Token),
-    t.union(...Object.values(SaberStyles))
-  ),
+  properties: t.optional(t.array(BaseLayerProperty), [
+    BaseLayerProperty.create({
+      title: "Color A",
+      token: ":colorA:",
+      value: Color.create({ value: "Blue" }),
+    }),
+  ]),
 })
 
 export default BaseLayer
