f9b17c640c08817707242528982206560de87483
Jordan Riser <riser.jordan@gmail.com>
2021-05-03 08:15:44 -0400

    wraps property editor in error boundary

--- src/components/PropertyEditor/index.jsx
+++ src/components/PropertyEditor/index.jsx
@@ -1,9 +1,11 @@
 import { Box, Typography } from "@material-ui/core"
 import classnames from "clsx"
+import ErrorFallback from "components/Common/ErrorFallback"
 import StylesContext from "contexts/Styles"
 import { isEmpty } from "lodash"
 import { observer } from "mobx-react"
 import { useContext } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 import { AutoSizer } from "react-virtualized"
 
 import EditProperty from "./EditProperty"
@@ -28,29 +30,31 @@ const PropertyEditor = () => {
       })}
       onClick={handleAreaClick}
     >
-      <Box p={3}>
-        {!isEmpty(store?.selectedLayer) && (
-          <>
-            <Typography variant="h5" className={classes.title}>
-              {store?.selectedLayer?.title}
-            </Typography>
-            <Box display="flex" flexDirection="column">
-              {store?.selectedLayer?.properties?.map((prop) => (
-                <PropertyInput key={prop.id} property={prop} />
-              ))}
-            </Box>
-          </>
-        )}
-      </Box>
-      <AutoSizer disableHeight style={{ width: "100%" }}>
-        {({ width }) => (
-          <EditProperty
-            width={width}
-            open={selectedProperty?.value?.canEdit}
-            value={selectedProperty?.value}
-          />
-        )}
-      </AutoSizer>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Box p={3}>
+          {!isEmpty(store?.selectedLayer) && (
+            <>
+              <Typography variant="h5" className={classes.title}>
+                {store?.selectedLayer?.title}
+              </Typography>
+              <Box display="flex" flexDirection="column">
+                {store?.selectedLayer?.properties?.map((prop) => (
+                  <PropertyInput key={prop.id} property={prop} />
+                ))}
+              </Box>
+            </>
+          )}
+        </Box>
+        <AutoSizer disableHeight style={{ width: "100%" }}>
+          {({ width }) => (
+            <EditProperty
+              width={width}
+              open={selectedProperty?.value?.canEdit}
+              value={selectedProperty?.value}
+            />
+          )}
+        </AutoSizer>
+      </ErrorBoundary>
     </Box>
   )
 }
