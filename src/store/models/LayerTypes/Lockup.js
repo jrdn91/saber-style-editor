fe00cc3490771db26e88723db692f8fc497fc43d
Jordan Riser <riser.jordan@gmail.com>
2021-05-12 20:51:12 -0400

    updates lockup values

--- src/store/models/LayerTypes/Lockup.js
+++ src/store/models/LayerTypes/Lockup.js
@@ -6,6 +6,7 @@ import Rgb from "store/models/ValueTypes/Rgb"
 import { v4 as uuidv4 } from "uuid"
 
 import { Layer, Property } from "../BaseModels"
+import SmoothStep from "../Functions/SmoothStep"
 import AudioFlicker from "../SaberStyles/AudioFlicker"
 
 const SaberStyles = {}
@@ -17,6 +18,17 @@ const saberStylesReq = require.context(
 saberStylesReq.keys().forEach((x) => {
   const saberStyleName = x.match(/\.\/([A-Za-z]+).js/)
   SaberStyles[saberStyleName[1]] = saberStylesReq(x).default
+})
+
+const Functions = {}
+const functionStylesReq = require.context(
+  "../Functions",
+  true,
+  /^(.*\.(js))[^.]*$/im
+)
+functionStylesReq.keys().forEach((x) => {
+  const functionStyleName = x.match(/\.\/([A-Za-z]+).js/)
+  Functions[functionStyleName[1]] = functionStylesReq(x).default
 })
 
 const ValueTypes = {}
@@ -36,9 +48,15 @@ const LockupProperty = Property.named("LockupProperty").props({
     Rgb,
     NumberModel,
     t.reference(Token),
-    t.union(...Object.values(SaberStyles))
+    t.union(...Object.values(SaberStyles)),
+    t.union(...Object.values(Functions))
   ),
 })
+
+const dragShapeSmoothStep = SmoothStep.create()
+console.log(dragShapeSmoothStep.toJSON())
+dragShapeSmoothStep.properties[0].value.update(28671)
+dragShapeSmoothStep.properties[1].value.update(4096)
 
 const Lockup = Layer.named("Lockup")
   .props({
@@ -66,11 +84,18 @@ const Lockup = Layer.named("Lockup")
         token: ":lockupShape:",
         value: NumberModel.create({ value: 32768 }),
       }),
+      LockupProperty.create({
+        title: "Drag shape",
+        token: ":dragShape:",
+        value: dragShapeSmoothStep,
+      }),
     ]),
   })
   .views((self) => ({
     get subLayers() {
-      return self.properties.filter((p) => p.value.type === "Style")
+      return self.properties.filter(
+        (p) => p.value.type === "Style" || p.value.type === "Function"
+      )
     },
   }))
 
